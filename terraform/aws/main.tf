7180976bc8a3fb410e883007a8897a0d44a6d4cf
Ozziekins <nenubariozioma@gmail.com>
2021-09-04 19:40:16 +0300

    Modified terraform config

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -18,6 +18,8 @@ variable "secret" {
 resource "aws_instance" "instance1" {
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
+  key_name = "devops"
+
   tags = {
     Name = "ubuntu-20.04"
   }
@@ -25,4 +27,25 @@ resource "aws_instance" "instance1" {
 
 output "url_server" {
   value = "http://${aws_instance.instance1.public_dns}"
+}
+
+output "SSH-command" {
+  value = "ssh -i ${aws_instance.instance1.key_name}.pem ubuntu@${aws_instance.instance1.public_dns}"
+}
+
+resource "aws_security_group" "main" {
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
