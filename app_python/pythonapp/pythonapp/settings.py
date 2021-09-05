b31607dc4415662019864bef1daebeb38fb2a734
Ozziekins <nenubariozioma@gmail.com>
2021-09-05 20:14:47 +0300

    modified app allowed_hosts to accept ec2 instances

--- app_python/pythonapp/pythonapp/settings.py
+++ app_python/pythonapp/pythonapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v%a5m=rrsrj3n8mizt+_p=)c4yx9$qmy1$fq@_6ib^4f-q@lo)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
