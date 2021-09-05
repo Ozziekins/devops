7c23e0022c95ea1bc44744f810c4fcff0e3ace58
Ozziekins <nenubariozioma@gmail.com>
2021-09-05 17:17:55 +0300

    modified terraform to collaborate with ansible

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -20,6 +20,8 @@ resource "aws_instance" "instance1" {
   instance_type = "t2.micro"
   key_name = "devops"
 
+  security_groups = [aws_security_group.allow-rules.name]
+
   tags = {
     Name = "ubuntu-20.04"
   }
@@ -33,13 +35,35 @@ output "SSH-command" {
   value = "ssh -i ${aws_instance.instance1.key_name}.pem ubuntu@${aws_instance.instance1.public_dns}"
 }
 
-resource "aws_security_group" "main" {
+resource "aws_security_group" "allow-rules" {
+  name        = "allow_all_rules"
+  description = "Allow inbound traffic from SSH and web"
+
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -47,5 +71,10 @@ resource "aws_security_group" "main" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-rules"
   }
 }
