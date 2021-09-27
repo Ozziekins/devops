9b51e9143e2d8e4fdd64308760e4d139efb33ea7
Ozziekins <nenubariozioma@gmail.com>
2021-09-27 18:43:21 +0300

    removed venv and added visits LOGGING

--- app_python/pythonapp/pythonapp/urls.py
+++ app_python/pythonapp/pythonapp/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('worldtime.urls')),
+    path('visits', include('worldtime.urls')),
 ]
--- app_python/pythonapp/worldtime/views.py
+++ app_python/pythonapp/worldtime/views.py
@@ -1,7 +1,14 @@
 import requests 
+from django.conf import settings 
 from django.shortcuts import render
 from datetime import datetime
 import pytz
+import logging 
+from pathlib import Path
+
+logger = logging.getLogger('visitlogger')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Create your views here.
 def index(request):
@@ -11,9 +18,27 @@ def index(request):
 
     # context = {'datetime' : r['datetime']}
 
+    logger.info('A client visited')
+    
     timezone_moscow = pytz.timezone('Europe/Moscow') 
     datetime_moscow = datetime.now(timezone_moscow)
 
     context = {'datetime' : datetime_moscow.strftime("%H:%M:%S")}
 
     return render(request, 'worldtime/index.html', context)
+
+
+def visits(request):
+
+    # datetime_now = datetime.now()
+
+    # context = {'visitime' : datetime_now.strftime("%H:%M:%S")}
+
+    short_report = open(BASE_DIR / 'logs/visits.log', 'rb')
+    response = HttpResponse(FileWrapper(short_report), content_type='text/plain')
+
+    # return response
+
+    context = {'visitlog' : response}
+
+    return render(request, 'worldtime/visits.html', context)
